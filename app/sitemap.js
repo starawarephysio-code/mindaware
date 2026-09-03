00c095a24158b0f2efee972958ead429abd28266
starawarephysio-code <starawarephysio@gmail.com>
2026-09-04 00:35:03 +0800

    feat: sitemap 加入 /experience 頁面

--- app/sitemap.js
+++ app/sitemap.js
@@ -11,6 +11,7 @@ export default function sitemap() {
   return [
     { url: "https://www.mindaware.tw", lastModified: new Date(), changeFrequency: "monthly", priority: 1 },
     { url: "https://www.mindaware.tw/faq", lastModified: new Date(), changeFrequency: "monthly", priority: 0.9 },
+    { url: "https://www.mindaware.tw/experience", lastModified: new Date(), changeFrequency: "yearly", priority: 0.7 },
     { url: "https://www.mindaware.tw/journal", lastModified: new Date(), changeFrequency: "weekly", priority: 0.8 },
     ...journalUrls,
     { url: "https://www.mindaware.tw/partnerships", lastModified: new Date(), changeFrequency: "yearly", priority: 0.5 },
